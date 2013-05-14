d5fd20cc5b4da69f1d0f23d951b37ce16d2ea15c
Energy Star <energystar.0124@gmail.com>
2013-05-14 22:40:49 +0800

    Basic setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DominionOnRails::Application.config.secret_token = 'e43ddf7e0085861c6b991b3f12d4164d01d69449d92d2a6b5bb031496af8c89d7ba2a5b889160b47a0d9dd512a9cc8887280d96de5720f4b357249dbbe728ad5'
